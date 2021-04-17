2bee70e9e5440a064f272f2023e547557cb7ff5f
Reggie Thompson <reggie.thompson4@gmail.com>
2021-04-17 15:07:01 -0600

    Add reducer, actions to update state of ids and artPieces

--- src/AppContext.js
+++ src/AppContext.js
@@ -10,6 +10,15 @@ const initialState = {
   error: ''
 }
 
+const reducer = (state, action) => {
+  switch(action.type) {
+    case 'GET_IDS':
+        return {...state, ids: action.payload}
+    case 'COLLECT_ARTPIECES': 
+      return {...state, artPieces: action.payload}
+  }
+}
+
 const AppProvider = (props) => {
   const [state, dispatch] = useReducer(reducer, initialState)
 
