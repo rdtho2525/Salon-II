f273963efd17696ac80cd0599928d2e9ab78ca74
Reggie Thompson <reggie.thompson4@gmail.com>
2021-04-17 15:21:40 -0600

    Introduce context, add button for favorites feature

--- src/components/Artwork/Artwork.js
+++ src/components/Artwork/Artwork.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import AppContext from '../../AppContext.js';
+import { useAppContext } from '../../AppContext.js';
 import { Link } from 'react-router-dom';
 import './Artwork.scss';
 import '../Wall/Wall.scss';
 
-const Artwork = ({ id, url, wallLocation }) => {
+const Artwork = ({ id, url, title, wallLocation }) => {
+  const [state, dispatch] = useAppContext();
 
   return (
     <Link to={`/${id}`} data={id} className={`img-container ${wallLocation}`}>
-      <img src={url} />
+      <img src={url} alt={`${title}_${id}`}/>
+      <button onClick={() => dispatch({ type: 'ADD_TO_FAVORITES', payload: id })}>Add to Favorites</button>
     </Link>
   )
 
