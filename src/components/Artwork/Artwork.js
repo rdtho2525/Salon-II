3b36943ca5f49398ff799fbd7b06eea872530cf5
Reggie Thompson <reggie.thompson4@gmail.com>
2021-04-19 08:47:00 -0600

    Pass artPiece object as payload, move favorite button out of link

--- src/components/Artwork/Artwork.js
+++ src/components/Artwork/Artwork.js
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useAppContext } from '../../AppContext.js';
 import { Link } from 'react-router-dom';
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+import FavoriteIcon from '@material-ui/icons/Favorite';
 import './Artwork.scss';
 import '../Wall/Wall.scss';
 
-const Artwork = ({ id, url, title, wallLocation }) => {
+const Artwork = ({ artPiece, id, url, title, wallLocation }) => {
+  const [isFavorited, setIsFavorited] = useState(false);
   const [state, dispatch] = useAppContext();
+  // const actionType = state.favoriteStatus === 'clear' ? 'ADD_TO_FAVORITES' : 'REMOVE_FROM_FAVORITES';
+  const checkFavStatus = () => {
+    return isFavorited ? <FavoriteIcon/> : <FavoriteBorderIcon/>
+  }
 
   return (
-    <Link to={`/${id}`} data={id} className={`img-container ${wallLocation}`}>
-      <img src={url} alt={`${title}_${id}`}/>
-      <button onClick={() => dispatch({ type: 'ADD_TO_FAVORITES', payload: id })}>Add to Favorites</button>
-    </Link>
+    <>
+      <button onClick={() => dispatch({ type: 'ADD_TO_FAVORITES', payload: artPiece })}>{checkFavStatus()}</button>
+      <Link to={`/${id}`} data={id} className={`img-container ${wallLocation}`}>
+        <img src={url} alt={`${title}_${id}`}/>
+      </Link>
+    </>
   )
 
 }
 
 export default Artwork;
+
+// src/assets/clear_heart.png
+// src/assets/clear_heart.png
