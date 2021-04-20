c47e8db2183d0fef27a63cc8109689a612b780ed
Reggie Thompson <reggie.thompson4@gmail.com>
2021-04-19 22:17:19 -0600

    RRRe-locate favorite button, import Tooltip component

--- src/components/Artwork/Artwork.js
+++ src/components/Artwork/Artwork.js
@@ -3,6 +3,9 @@ import { useAppContext } from '../../AppContext.js';
 import { Link } from 'react-router-dom';
 import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 import FavoriteIcon from '@material-ui/icons/Favorite';
+import { Tooltip } from '@material-ui/core';
+import FavoriteButton from '../FavoriteButton/FavoriteButton.js';
+
 import './Artwork.scss';
 import '../Wall/Wall.scss';
 
@@ -10,25 +13,44 @@ const Artwork = ({ artPiece, id, url, title, wallLocation }) => {
   const [isFavorited, setIsFavorited] = useState(false);
   const [state, dispatch] = useAppContext();
   const actionType = isFavorited === false ? 'ADD_TO_FAVORITES' : 'REMOVE_FROM_FAVORITES';
+
+  const style = {
+    fontSize: 25,
+    marginTop: 5,
+    cursor: "pointer"
+  }
   
   const toggleFavorite = () => {
     isFavorited === false ? setIsFavorited(true) : setIsFavorited(false);
   }
 
   const checkFavStatus = () => {
-    return isFavorited ? <FavoriteIcon/> : <FavoriteBorderIcon/>
+    if (isFavorited) {
+      return (
+        <Tooltip title="Remove from Favorites">
+          <FavoriteIcon aria-label={"Remove from Favorites"} style={style}/> 
+        </Tooltip>
+      )
+    } else {
+      return (
+        <Tooltip title="Add to Favorites">
+          <FavoriteBorderIcon aria-label={"Add to Favorites"} style={style}/>
+        </Tooltip>
+      )
+    }
   }
 
   return (
-    <>
+    <section className="landing-section">
       <button onClick={() => {
         dispatch({ type: actionType, payload: artPiece })
         toggleFavorite()
       }}>{checkFavStatus()}</button>
+      <FavoriteButton/>
       <Link to={`/${id}`} data={id} className={`img-container ${wallLocation}`}>
         <img src={url} alt={`${title}_${id}`}/>
       </Link>
-    </>
+    </section>
   )
 
 }
